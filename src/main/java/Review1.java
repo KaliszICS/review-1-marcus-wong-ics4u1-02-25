bcbcf542d54e2ac9bf95d1e72f4c32ee200f96a6
marcus-wong-ics4u1-02-25 <marcus.wong5@student.tdsb.on.ca>
2025-02-04 15:22:05 +0000

    sae assignment

--- src/main/java/Review1.java
+++ src/main/java/Review1.java
@@ -2,22 +2,40 @@ public class Review1 {
 
 	public static void q1() {
 		//Write question 1 code here
+		System.out.println("There once was a man from St. Ives.");
 	}
 
 	public static void q2() {
 		//Write question 2 code here
+		int num;
+		int num2;
+		num = 9;
+		num2 = 5;
+		System.out.println(num / num2);
+		System.out.println(num % num2);
+
+
 	}
 
 	public static void q3() {
 		//Write question 3 code here
+		System.out.println("Hello\nHello again");
 	}
 
 	public static void q4() {
 		//Write question 4 code here
+		boolean trueFalse = true;
+		System.out.println(trueFalse);
 	}
 
 	public static void q5() {
 		//Write question 5 code here
+		String word;
+		String word2;
+		word = "Hello";
+		word2 = "World";
+		System.out.print(word + ' ');
+		System.out.println(word2);
 	}
 
 }
